a642adc05ad897589dd2bbb418ef1ce5f1ae8493
Tannor Wallace <tannor.wallace@gmail.com>
2022-02-16 17:57:31 -0700

    Walking Skeleton Complete

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,7 +32,8 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
-         
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,13 +42,13 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                           
+
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
