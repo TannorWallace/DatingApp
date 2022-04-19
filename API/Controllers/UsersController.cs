aa15ea64299a235e97647e862286e3bafdf8c9e0
Tannor Wallace <tannor.wallace@gmail.com>
2022-04-19 16:42:28 -0600

    lots covered

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -22,25 +22,20 @@ namespace API.Controllers
         public UsersController(IUserRepository userRepository, IMapper mapper)
         {
             _mapper = mapper;
-            _userRepository = userRepository;
-            
+            _userRepository = userRepository;            
         }
 
         [HttpGet]
-
         public async Task <ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
             var users = await _userRepository.GetMembersAsync();
-            // var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
             return Ok(users);
         }
 
         [HttpGet("{username}")]
         public async Task <ActionResult<MemberDto>> GetUser(string username)
         {
-            // var user = 
             return await _userRepository.GetMemberAsync(username);
-            // return _mapper.Map<MemberDto>(user);
         }
     }
 }
---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+//temporary
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemebersService {
+  baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
+  
+  getMembers(){
+    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions)
+  }
+}
