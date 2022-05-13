a3e9a83edc483abcfdcea638a8ab869d6be0be51
Tannor Wallace <tannor.wallace@gmail.com>
2022-05-13 16:14:58 -0600

    Photos and main

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -81,7 +81,27 @@ namespace API.Controllers
                 return CreatedAtRoute("GetUser", new { username = user.UserName }, _mapper.Map<PhotoDto>(photo));
 
             }
-            return BadRequest("Problem Adding Photo");
+            return BadRequest("Problem Adding Pict");
+        }
+
+        [HttpPut("set-main-photo/{photoId}")]
+        public async Task<ActionResult> SetMainPhoto(int photoId)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if(photo.IsMain) return BadRequest("This is already your main Pict");
+
+            var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+            if (currentMain != null) currentMain.IsMain = false;
+            photo.IsMain = true;
+
+            if (await _userRepository.SaveAllAsync())
+            {
+                return NoContent();
+            }
+            return BadRequest("Failed to Set Main Pict");
         }
     }
 }
