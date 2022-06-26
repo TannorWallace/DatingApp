a2e129de6bc43661bcf0244b839d28a80e92eecf
Tannor Wallace <tannor.wallace@gmail.com>
2022-06-26 12:44:42 -0600

    date formatting update API end Broken?

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -25,6 +25,8 @@ export class MemebersService {
     params = params.append('minAge', userParams.minAge.toString());
     params = params.append('maxAge', userParams.maxAge.toString());
     params = params.append('gender', userParams.gender);
+    params = params.append('orderBy', userParams.orderBy);
+
 
 
     return this.getPaginatedResult<Member[]>(this.baseUrl + 'users',params)
