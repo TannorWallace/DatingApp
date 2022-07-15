ed1abae40f847f87ebf918fd0a23059cc2488a51
Tannor Wallace <tannor.wallace@gmail.com>
2022-07-15 15:11:24 -0600

    Backend like functionallity working next, client

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -13,5 +13,26 @@ namespace API.Data
         {
         }
         public DbSet<AppUser> Users{ get; set; }//Name Of the Table
+        public DbSet<UserLike> Likes { get; set; }//Table Name for likes
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(k => new { k.SourceUserId, k.LikedUserId });
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
